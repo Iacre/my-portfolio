93fb69e97a44685bd02625837a6fcc5234d62708
Ishukwe Fiacre <fiacre@srv620094.hstgr.cloud>
2025-01-31 23:53:02 +0000

    tt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,49 +12,49 @@ import Subscription from "./components/Subscription";
 import Contact from "./components/Contact";
 import Footer from "./components/footer";
 import TestimonialCarousel from "./components/TestimonialCarousel";
-import CV from "./components/cv";
+import CV from "./components/CV";
 
 
 
 
 
 const tracks = [
-  { title: "Track 1", url: "./src/assets/musics/akon_smack_that_.mp3" },
-  { title: "Track 2", url: "./src/assets/musics/ariel_wayz_juno_kizigenza_away.mp3" },
-  { title: "Track 3", url: "./src/assets/musics/beyonce_halo_.mp3" },
-  { title: "Track 4", url: "./src/assets/musics/coldplay_fix_you.mp3" },
-  { title: "Track 5", url: "./src/assets/musics/ed_sheeran_perfect.mp3" },
-  { title: "Track 6", url: "./src/assets/musics/eminem_love_the_way_you_lie.mp3" },
-  { title: "Track 7", url: "./src/assets/musics/hello.mp3" },
-  { title: "Track 8", url: "./src/assets/musics/khalid_young_dumb.mp3" },
-  { title: "Track 9", url: "./src/assets/musics/lady_gaga_bad_romance.mp3" },
-  { title: "Track 10", url: "./src/assets/musics/lauren_daigle_you_say.mp3" },
-  { title: "Track 11", url: "./src/assets/musics/lil_nas_x_old_town_road.mp3" },
-  { title: "Track 12", url: "./src/assets/musics/oceans_where_feet_may_fail.mp3" },
-  { title: "Track 13", url: "./src/assets/musics/post_malone_swae_lee_sunflower.mp3" },
-  { title: "Track 14", url: "./src/assets/musics/praise_feat._brandon_lake_chris_brown_chandler.mp3" },
-  { title: "Track 15", url: "./src/assets/musics/rema_calm_down.mp3" },
-  { title: "Track 16", url: "./src/assets/musics/rihanna_man_down.mp3" },
-  { title: "Track 17", url: "./src/assets/musics/sia_snowman.mp3" },
-  { title: "Track 18", url: "./src/assets/musics/sia_unstoppable.mp3" },
-  { title: "Track 19", url: "./src/assets/musics/wiz_khalifa_see_you_again.mp3"}
+  { title: "Track 1", url: "./assets/musics/akon_smack_that_.mp3" },
+  { title: "Track 2", url: "./assets/musics/ariel_wayz_juno_kizigenza_away.mp3" },
+  { title: "Track 3", url: "./assets/musics/beyonce_halo_.mp3" },
+  { title: "Track 4", url: "./assets/musics/coldplay_fix_you.mp3" },
+  { title: "Track 5", url: "./assets/musics/ed_sheeran_perfect.mp3" },
+  { title: "Track 6", url: "./assets/musics/eminem_love_the_way_you_lie.mp3" },
+  { title: "Track 7", url: "./assets/musics/hello.mp3" },
+  { title: "Track 8", url: "./assets/musics/khalid_young_dumb.mp3" },
+  { title: "Track 9", url: "./assets/musics/lady_gaga_bad_romance.mp3" },
+  { title: "Track 10", url: "./assets/musics/lauren_daigle_you_say.mp3" },
+  { title: "Track 11", url: "./assets/musics/lil_nas_x_old_town_road.mp3" },
+  { title: "Track 12", url: "./assets/musics/oceans_where_feet_may_fail.mp3" },
+  { title: "Track 13", url: "./assets/musics/post_malone_swae_lee_sunflower.mp3" },
+  { title: "Track 14", url: "./assets/musics/praise_feat._brandon_lake_chris_brown_chandler.mp3" },
+  { title: "Track 15", url: "./assets/musics/rema_calm_down.mp3" },
+  { title: "Track 16", url: "./assets/musics/rihanna_man_down.mp3" },
+  { title: "Track 17", url: "./assets/musics/sia_snowman.mp3" },
+  { title: "Track 18", url: "./assets/musics/sia_unstoppable.mp3" },
+  { title: "Track 19", url: "./assets/musics/wiz_khalifa_see_you_again.mp3"}
   
 ];
 const testimonials = [
   {
-     image: "./src/assets/tes.png",
+     image: "./assets/tes.png",
      name: "John Doe",
      position: "CEO, Example Corp",
      testimonial:"FIacre's work has been transformative for our business. His innovative solutions and creative designs have significantly improved our operations and brand presence.",
   },
   {
-     image: "./src/assets/tes.png",
+     image: "./assets/tes.png",
      name: "Jane Smith",
      position: "CTO, Tech Innovators",
      testimonial:"Working with FIacre has been a game-changer. His expertise in AI and machine learning has propelled our projects to new heights.",
   },
   {
-     image: "./src/assets/tes.png",
+     image: "./assets/tes.png",
      name: "Michael Johnson",
      position: "Creative Director, Design Studio",
      testimonial:"FIacre's creative vision and attention to detail are unparalleled. His designs have brought our brand to life in ways we never imagined.",
@@ -66,7 +66,7 @@ function App() {
     <div className="App bg-mypri min-h-screen w-full relative">
       {/* Background Image */}
       <div 
-        className="absolute inset-0 bg-[url('./src/assets/tx.png')] bg-repeat bg-cover bg-center opacity-5"
+        className="absolute inset-0 bg-[url('./assets/tx.png')] bg-repeat bg-cover bg-center opacity-5"
         aria-hidden="true"
       ></div>
 
@@ -88,7 +88,7 @@ function App() {
             title="FIacre: Innovating Solutions, Crafting the Future"
             description="CEO, Software Developer, Cybersecurity Expert, AI & ML Specialist, and Creative Designer
 Welcome to my world of innovation and creativity. As the CEO of EdgeReach Tech, I specialize in developing cutting-edge software solutions, ensuring cybersecurity, and leveraging AI and  ML to solve complex problems. With a passion for design and a focus on creativity, I transform ideas into impactful, real-world applications. Explore my portfolio to discover how I merge technology and creativity to deliver unique solutions."
-            image="./src/assets/herofia1.png"
+            image="./assets/herofia1.png"
             primaryButtonText="Contact Me"
             secondaryButtonText="My Resume"
            
@@ -105,7 +105,7 @@ Welcome to my world of innovation and creativity. As the CEO of EdgeReach Tech, 
   title="FIacre: The Solution to Your Problems, Bridging Gaps, and Unveiling Mysteries"
   description="Transforming Businesses and Enhancing Lives with Innovative Solutions
 In the ever-evolving world of technology and design, I am here to be the solution to your problems, filling the gaps and uncovering the secrets that will elevate your business and life. With expertise in software development, cybersecurity, AI, ML, and creative design, I am dedicated to making your vision a reality. Let’s explore how my innovative approach can make a lasting impact on your success."
-  image="./src/assets/a.png"
+  image="./assets/a.png"
   primaryButtonText="Learn More"
   onPrimaryClick={() => alert("Contact Me clicked!")}
   />
@@ -122,25 +122,25 @@ heading="Skills And Experiences"
           <div className="mb-8">
             <h3 className="text-myhigh mb-4">Technical Development Skills</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-              <SkillCard skill="Software Development" image="./src/assets/sod.jpg" />
-              <SkillCard skill="AI and Machine Learning" image="./src/assets/ai.jpg" />
-              <SkillCard skill="Cybersecurity" image="./src/assets/cyb.jpg" />
+              <SkillCard skill="Software Development" image="./assets/sod.jpg" />
+              <SkillCard skill="AI and Machine Learning" image="./assets/ai.jpg" />
+              <SkillCard skill="Cybersecurity" image="./assets/cyb.jpg" />
             </div>
           </div>
           <div className="mb-8">
             <h3 className="text-myhigh mb-4">Creative Design Skills</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-              <SkillCard skill="UI/UX Design" image="./src/assets/ux.jpg" />
-              <SkillCard skill="Graphic Design" image="./src/assets/de.jpg" />
-              <SkillCard skill="Web Design" image="./src/assets/wb.jpg" />
+              <SkillCard skill="UI/UX Design" image="./assets/ux.jpg" />
+              <SkillCard skill="Graphic Design" image="./assets/de.jpg" />
+              <SkillCard skill="Web Design" image="./assets/wb.jpg" />
             </div>
           </div>
           <div className="mb-4">
           <h3 className="text-myhigh mb-4">Business and Management skills</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-          <SkillCard skill="Leadership " image="./src/assets/led.jpg" />
-          <SkillCard skill="Inovation & problem solving" image="./src/assets/cre.jpg" />
-          <SkillCard skill="project Management" image="./src/assets/pr.jpg" />
+          <SkillCard skill="Leadership " image="./assets/led.jpg" />
+          <SkillCard skill="Inovation & problem solving" image="./assets/cre.jpg" />
+          <SkillCard skill="project Management" image="./assets/pr.jpg" />
           </div>
         </div>
 </div>
@@ -167,21 +167,21 @@ heading="Blogs"
 <div id="blog" className="px-2 md:px-8 py-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <Blog
-              image="./src/assets/b3.png"
+              image="./assets/b3.png"
               date="May 25, 2021"
               heading="The Future of AI and Machine Learning"
               paragraph="Discover the latest trends in AI and machine learning and how they are shaping the future."
               link="https://google.com"
             />
             <Blog
-              image="./src/assets/b1.png"
+              image="./assets/b1.png"
               date="June 15, 2021"
               heading="The Impact of Cybersecurity"
               paragraph="Learn how cybersecurity is essential for businesses and how it can protect your data and assets."
               link="https://google.com"
             />
             <Blog
-              image="./src/assets/b2.png"
+              image="./assets/b2.png"
               date="July 5, 2021"
               heading="The Art of Creative Design"
               paragraph="Explore the world of creative design and how it can transform your business and brand."
@@ -203,7 +203,7 @@ heading="CV"
 />
 </div>
  {/* resume button */}
- <div id="resume"><AnimatedButton text="Download My Resume" onClick={() => {}}  cvUrl="./src/assets/Fiacre ISHUKWE CV.pdf"/></div>
+ <div id="resume"><AnimatedButton text="Download My Resume" onClick={() => {}}  cvUrl="./assets/Fiacre ISHUKWE CV.pdf"/></div>
 
   {/* resume SECTION */}
   {/* title div */}
